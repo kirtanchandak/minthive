5331adc78a17c9b95f13b08a7af123662fdafad2
kirtanchandak <themarketingkirtan@gmail.com>
2024-05-31 17:57:36 +0530

    dummy siginin logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,11 @@
 import express from "express";
+import userRouter from "./routers/user";
+import workerRouter from "./routers/worker";
+import { Router } from "express";
 
 const app = express();
+
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);
+
+app.listen(3000);
---
+++ backend/src/routers/user.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = "chandak";
+
+const router = Router();
+
+const prismaClient = new PrismaClient();
+
+router.post("/signin", async (req, res) => {
+  const hardCodedWalletAddress = "0x3D68b6bE0fA7aeea256cef433373B5a81348ab4C";
+
+  const existingUser = await prismaClient.user.findFirst({
+    where: {
+      address: hardCodedWalletAddress,
+    },
+  });
+
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        userId: existingUser.id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token,
+    });
+  } else {
+    const user = await prismaClient.user.create({
+      data: {
+        address: hardCodedWalletAddress,
+      },
+    });
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token,
+    });
+  }
+});
+
+export default router;

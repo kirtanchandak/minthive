c1cc006c1e69a6a6cc215cd9a6d1d172622713ff
kirtanchandak <themarketingkirtan@gmail.com>
2024-06-02 13:06:46 +0530

    solana wallet adapter added

--- user-frontend/src/App.tsx
+++ user-frontend/src/App.tsx
@@ -1,19 +1,44 @@
 import "./App.css";
+import { useMemo } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NewTask from "./pages/NewTask";
 import AppBar from "./components/AppBar";
 import TaskDetails from "./pages/TaskDetails";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [], [network]);
   return (
     <>
-      <AppBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/newTask" element={<NewTask />} />
-        <Route path="/task/:taskId" element={<TaskDetails />} />
-      </Routes>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <AppBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/newTask" element={<NewTask />} />
+              <Route path="/task/:taskId" element={<TaskDetails />} />
+            </Routes>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   );
 }
